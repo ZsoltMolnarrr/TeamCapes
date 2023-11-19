dc64acee1382d79ae6468d3291ecff6e76dbade6
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-11-19 13:08:42 +0100

    Update Forge related

--- forge/src/main/java/net/team_capes/teamcapes/forge/TeamCapesForge.java
+++ forge/src/main/java/net/team_capes/teamcapes/forge/TeamCapesForge.java
@@ -1,15 +1,13 @@
 package net.team_capes.teamcapes.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.team_capes.TeamCapesMod;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TeamCapesMod.NAMESPACE)
 public class TeamCapesForge {
     public TeamCapesForge() {
 		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(TeamCapesMod.NAMESPACE, FMLJavaModLoadingContext.get().getModEventBus());
+        // EventBuses.registerModEventBus(TeamCapesMod.NAMESPACE, FMLJavaModLoadingContext.get().getModEventBus());
         TeamCapesMod.init();
     }
 }
